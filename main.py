481782775f051970258f69986ce2253e5edbf6c8
SrutiDash <91628154+SrutiDash@users.noreply.github.com>
2025-03-18 20:41:17 +0000

    few lines

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Hello World!")
+print('''hii
+this
+is
+sruti''')
+# we can use triple single quotation(''' xyz ''') or triple double quotation(""" xyz """) for printing something or sentence
+# in different lines
+# or we can use /n this is more readable
+print('hi \nthis \nis \nsruti')
+print('it\'s a nice day')
+# using \ if there's an ' then it can print as it is
