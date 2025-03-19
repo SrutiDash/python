17a49267d893c60bcde6ac4b7d29c86d33f29af4
SrutiDash <91628154+SrutiDash@users.noreply.github.com>
2025-03-19 16:29:39 +0000

    user input

---
+++ variables.py
@@ -0,0 +1,12 @@
+a = 'sruti'
+print(a)
+
+# user input
+# for string no data type has to be used
+name=input('enter your name : ')
+print(name)
+# for integer , int has to be used
+age = int(input('enter your age : '))
+print(age)
+pi = float(input('enter the value of pi : '))
+print(pi)
