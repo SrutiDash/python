ceb50a665716976bb4092da15cff44ddd43565d1
SrutiDash <91628154+SrutiDash@users.noreply.github.com>
2025-03-19 16:34:54 +0000

    evaluate

--- variables.py
+++ variables.py
@@ -8,5 +8,10 @@ print(name)
 # for integer , int has to be used
 age = int(input('enter your age : '))
 print(age)
+# for float, float has to be used
 pi = float(input('enter the value of pi : '))
 print(pi)
+# evaluate
+
+exp = eval(input('enter any equation here : '))
+print(exp)
