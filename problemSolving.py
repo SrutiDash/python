d9e60f50efb03f575f91f4c704270ee174c30336
SrutiDash <91628154+SrutiDash@users.noreply.github.com>
2025-03-19 17:13:36 +0000

    few solutions

---
+++ problemSolving.py
@@ -0,0 +1,48 @@
+# problem solving
+# 1
+#name, age, address
+name = input("enter your name : ")
+print(name)
+age = input("enter your age : ")
+print(age)
+address = input("enter your address : ")
+print(address)
+# 2
+# swap two variables
+a=23
+b=34
+print(a,b)
+c=a
+a=b
+b=c
+print(a,b)
+#3
+#float to int
+a=34
+b=23.3
+print(type(a))
+print(type(b))
+b=int(b)
+print("after typecasting float to int : ", type(b))
+#4
+#different details from student's id card
+id=input("enter your id : ")
+print(id)
+name=input("enter your name : ")
+print(name)
+grade=input("enter your grade : ")
+print(grade)
+bloodType=input("enter your bloodType : ")
+print(bloodType)
+age=input("enter your age : ")
+print(age)
+address=input("enter your address : ")
+print(address)
+#5
+# user input and then convert to float
+num=input('enter the value of num : ')
+print(num)
+print(type(num))
+num=float(num)
+print(type(num))
+print(num)
